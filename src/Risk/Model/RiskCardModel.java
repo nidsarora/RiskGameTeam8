5494177c450f0fa2122b6abf2dcc6e50c63ae9d7
Nidhi Arora <aroranidhi10@gmail.com>
2017-10-05 13:02:41 -0400

    Added MVC pattern for the project

---
+++ src/Risk/Model/RiskCardModel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Risk.Model;
+
+
+public class RiskCardModel {
+
+    public int value;
+    public int territory;
+
+    RiskCardModel(int v, int t){
+        territory = t;
+        value = v;
+    }
+
+}
---
+++ src/Risk/Model/RiskContinentModel.java
@@ -0,0 +1,49 @@
+package Risk.Model;
+
+import java.util.Vector;
+
+
+public class RiskContinentModel {
+	
+	private String name;
+	private Vector<Integer> territories;
+	private int value;
+	RiskTerritoryModel t;
+	
+	
+	RiskContinentModel(String nm, Vector<Integer> t, int v){
+		name = nm;
+		value = v;
+		territories = t;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public Vector<Integer> getTerritories(){
+		return territories;
+	}
+	
+	public int getValue(){
+		return value;
+	}
+	
+	public boolean isTerritoryOf(RiskTerritoryModel t){
+		return (territories.contains(t.getId()));
+	}
+	
+	public boolean isContinentCaptured(RiskPlayerModel p){
+		Vector<Integer> t1 = new Vector<Integer>();
+		Vector<RiskTerritoryModel> t2 = p.getOccupiedTerritories();
+                
+		for (int c = 0; c < t2.size(); c++)
+			t1.add(t2.elementAt(c).getId());              
+		for (int i = 0; i < territories.size(); i++){
+                    if(!t1.contains(territories.elementAt(i)))
+			return false;
+		}//end for loop		
+	return true; 
+	}
+
+}
