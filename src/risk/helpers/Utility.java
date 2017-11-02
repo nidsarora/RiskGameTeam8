256b4242b7481ae706039bd58c77b15304039aa0
Goutham Raj <genialguy1@gmail.com>
2017-11-02 00:54:44 -0400

    changes for logger

--- src/risk/helpers/Utility.java
+++ src/risk/helpers/Utility.java
@@ -1,27 +1,39 @@
 package risk.helpers;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
- *This class is used to get the path of the resources used in the project.
+ * This class is used to get the path of the resources used in the project.
  */
 public final class Utility {
-	
-	public static String getImagePath(String imageNameWithExtention)
-	{
+
+	public static String getImagePath(String imageNameWithExtention) {
 		return "/risk/resources/images/" + imageNameWithExtention;
 	}
-	
-	public static String getMapPath(String mapNameWithExtension)
-	{
+
+	public static String getMapPath(String mapNameWithExtension) {
 		return "/risk/resources/map/" + mapNameWithExtension;
 	}
-	
-	public static String getMapPathforFile(String mapNameWithExtension)
-	{
+
+	public static String getMapPathforFile(String mapNameWithExtension) {
 		return "src/risk/resources/map/" + mapNameWithExtension;
 	}
-	
-	public static String getPathforFile(String fileNameWithExtension)
-	{
+
+	public static String getPathforFile(String fileNameWithExtension) {
 		return "src/risk/resources/" + fileNameWithExtension;
+	}
+
+	public static void writeLog(String text) {
+		try (FileWriter fw = new FileWriter("log\\log.txt", true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw)) {
+			out.println(text);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
