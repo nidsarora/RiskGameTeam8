39ddbd033031505c24778b1c88b7fed0f1fc2417
Kishor <tare.kishor@gmail.com>
2017-11-26 14:10:22 -0500

    Modified Benevolent strategy for the Comp player
    Signed-off-by: Kishor <tare.kishor@gmail.com>

--- src/risk/model/strategy/Benevolent.java
+++ src/risk/model/strategy/Benevolent.java
@@ -8,57 +8,37 @@ import risk.model.interfaces.StrategyInterface;
  */
 public class Benevolent implements StrategyInterface {
 	
-	/* (non-Javadoc)
-	 * @see risk.model.interfaces.StrategyInterface#startTurn(boolean, risk.model.RiskGameModel)
-	 */
 	@Override
 	public void startTurn(boolean isTest, RiskGameModel riskGameModel) {
-		// TODO Auto-generated method stub
+		
 		return;
 	}
 
-	/* (non-Javadoc)
-	 * @see risk.model.interfaces.StrategyInterface#reinforce(boolean, risk.model.RiskGameModel, int[])
-	 */
-	@Override
+		@Override
 	public void reinforce(boolean isTest, RiskGameModel riskGameModel, int... territory) {
-		// TODO Auto-generated method stub
+		
 		return;
 	}
 
-	/* (non-Javadoc)
-	 * @see risk.model.interfaces.StrategyInterface#attack(boolean, risk.model.RiskGameModel, int[])
-	 */
-	@Override
+		@Override
 	public void attack(boolean isTest, RiskGameModel riskGameModel, int... territory) {
-		// TODO Auto-generated method stub
+		
 		return;
 	}
 
-	/* (non-Javadoc)
-	 * @see risk.model.interfaces.StrategyInterface#fortify(boolean, risk.model.RiskGameModel, int[])
-	 */
-	@Override
+		@Override
 	public void fortify(boolean isTest, RiskGameModel riskGameModel, int... territory) {
-		// TODO Auto-generated method stub
+		
 		return;
 	}
 
-	/* (non-Javadoc)
-	 * @see risk.model.interfaces.StrategyInterface#initialReinforce(boolean, risk.model.RiskGameModel, int[])
-	 */
-	@Override
+		@Override
 	public void initialReinforce(boolean isTest, RiskGameModel riskGameModel, int... territory) {
-		// TODO Auto-generated method stub
 		return;
 	}
 
-	/* (non-Javadoc)
-	 * @see risk.model.interfaces.StrategyInterface#takeTurn(boolean, risk.model.RiskGameModel)
-	 */
-	@Override
+		@Override
 	public void takeTurn(boolean isTest, RiskGameModel riskGameModel) {
-		// TODO Auto-generated method stub
 		return;
 	}
 
