a9604259823c28b789ed8b061a9133674a1db5eb
nidsarora <aroranidhi10@gmail.com>
2017-11-30 00:09:17 -0500

    Revert "fixed userdefined map bug"
    This reverts commit 428282418f3574150a33bf88cac3b978e089d625.

--- src/risk/helpers/Utility.java
+++ src/risk/helpers/Utility.java
@@ -226,7 +226,6 @@ public final class Utility {
 			out.println("***Game Stats***\n\n");
 
 			if (riskGameModel != null)
-			{
 				out.println("Map is " + riskGameModel.currentTournamentGameMapName);
 				for (RiskPlayerModel player : RiskGameModel.players) {
 					out.println(player.getName() + " T Count " + player.getOccupiedTerritories().size() + " Army Count "
@@ -236,7 +235,6 @@ public final class Utility {
 					}
 					out.println("\n\n\n");
 				}
-			}
 			out.close();
 			bw.close();
 			fw.close();
